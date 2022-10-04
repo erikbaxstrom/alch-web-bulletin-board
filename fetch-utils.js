8b44cde73fa4ffa2e2f9c1140ab056da50d905c7
Erik M Baxstrom <baxstrem@gmail.com>
2022-10-04 09:29:10 -0700

    add API keys

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://vhggynhjqqqtwnroniln.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZ2d5bmhqcXFxdHducm9uaWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4NDQyMTYsImV4cCI6MTk4MDQyMDIxNn0.LywedVmTirq5Yt63NAh6Y7aUjEb_3Q4tzwYTQAl2Nu8';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
