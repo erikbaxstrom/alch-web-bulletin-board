4dc2679f848b10330780b65399b4ba4e9c9deb95
Erik M Baxstrom <baxstrem@gmail.com>
2022-10-04 11:21:32 -0700

    add error display

--- fetch-utils.js
+++ fetch-utils.js
@@ -29,7 +29,6 @@ export async function signOutUser() {
 
 /* Data functions */
 export async function createPost(post) {
-    // await console.log(post);
     const response = client.from('posts').insert(post).single();
     return response;
 }
